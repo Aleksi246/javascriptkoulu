5fd5039eb06104f5dcfdbe39346e840a5f5ecea5
Aleksipc <aleklap@metropolia.figit config --global user.emailgit config --global user.email aleklap@metropolia.fi>
2024-11-08 03:21:54 +0200

    asd

--- oh2jsm1/tehtävä8.js
+++ oh2jsm1/tehtävä8.js
@@ -1,3 +1,26 @@
-const start = Number(prompt("start year"))
-const  end = Number(prompt("end year"))
+const start = Number(prompt("start year"));
+const  end = Number(prompt("end year"));
 
+let joku1 = ``;
+
+for(let i = start; i <= end; i++){
+
+    let arvo = false;
+    if(i%4 === 0){
+
+        if(i%100 === 0){
+            if(i%400 ===0){
+                arvo = true
+            }
+        }
+        else {
+             arvo = true
+        }
+    }
+    if(arvo === true){
+        let listaitem = `<li>${i}</li>`
+        joku1 +=  listaitem
+    }
+}
+
+document.querySelector("#target").innerHTML = `<ul>${joku1}</ul>`
